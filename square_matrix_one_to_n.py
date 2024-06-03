2f5696a98f1e22db614b097d717cbf45ac756705
Rajvardhan <rajvardhantekawade@gmail.com>
2024-06-03 23:07:57 +0530

    Square Matrix Verification

---
+++ square_matrix_one_to_n.py
@@ -0,0 +1,14 @@
+#The test case for n=19 isn't working (reason: The LeetCode answerkey switches the expected output between true and false)
+class Solution:
+    def checkValid(self, matrix: list[list[int]]) -> bool:
+        
+        l=b=0
+        t=[]
+        for i in matrix[0]:
+            l+=1
+            t.append(l)
+        for i in matrix:
+            i.sort()
+            if i!=t:
+                return False
+        return True

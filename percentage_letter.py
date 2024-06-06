00de5581592de805da2dd94c424475dc67fc2619
Rajvardhan-Tekawade <145952941+Rajvardhan-Tekawade@users.noreply.github.com>
2024-06-06 16:31:02 +0530

    Optimized

--- percentage_letter.py
+++ percentage_letter.py
@@ -1,7 +1,5 @@
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
         c=s.count(letter)
-        l=0
-        for i in s:
-           l+=1
+        l=len(s)
         return int(((c/l)*100))

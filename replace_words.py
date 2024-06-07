1c2b66a73a4cdc3479a99ac122c5ca6dbc126d18
Rajvardhan-Tekawade <145952941+Rajvardhan-Tekawade@users.noreply.github.com>
2024-06-07 18:22:08 +0530

    Dictionary root swap

---
+++ replace_words.py
@@ -0,0 +1,11 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence=sentence.split(" ")
+        for i in dictionary:
+            for j in range(len(sentence)):
+                if i == sentence[j][0:len(i)]:
+                    sentence[j]=i
+        r=""
+        for i in sentence:
+           r=r+i+" "
+        return (r[:-1])
